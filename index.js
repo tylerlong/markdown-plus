ec4955be0086104dc08a69222df4e06614904e3e
Tyler Long <tyler4long@gmail.com>
2015-03-11 14:11:44 +0800

    Add marked

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ $(document).ready(function () {
   });
 
   var editor = ace.edit("editor");
+  editor.$blockScrolling = Infinity;
   editor.getSession().setMode("ace/mode/markdown");
   editor.renderer.setShowPrintMargin(false);
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+  });
+
+  editor.session.on('change', function(){
+    $('.ui-layout-east').html(marked(editor.session.getValue()));
+  });
+
 });
