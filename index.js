7a5e3f841a51e4c3b033e91be1af8e0f8c9eed35
Tyler Long <tyler4long@gmail.com>
2015-03-12 13:17:50 +0800

    Tweak UI

--- index.js
+++ index.js
@@ -5,24 +5,31 @@ $(document).ready(function () {
 
   // 构造上中右三个面板
   myLayout = $('body').layout({
+    resizerDblClickToggle: false,
+    resizable: false,
+    slidable: false,
+    togglerLength_open: '100%',
+    togglerLength_closed: '100%',
     center: {
       childOptions: {
+        resizerDblClickToggle: false,
+        resizable: false,
+        slidable: false,
+        togglerLength_open: '100%',
+        togglerLength_closed: '100%',
         north: {
           size: '1px', // 只是占位，真实大小由内容决定
-          resizable: false,
           togglerTip_open: 'Hide Toolbar',
           togglerTip_closed: 'Show Toolbar'
-        },
+        }
       }
     },
     east: {
       size: '50%',
-      resizable: false,
       togglerTip_open: 'Hide Preview',
       togglerTip_closed: 'Show Preview',
       onresize: function(){
-        // ACE的wrap貌似有问题，这里手动触发一下。
-        editor.session.setUseWrapMode(false);
+        editor.session.setUseWrapMode(false); // ACE的wrap貌似有问题，这里手动触发一下。
         editor.session.setUseWrapMode(true);
       }
     }
@@ -40,15 +47,15 @@ $(document).ready(function () {
   // 设置marked
   var renderer = new marked.Renderer();
   renderer.listitem = function(text) {
-    var taskListRegex1 = /^\[ \]\s/
-    var taskListRegex2 = /^\[x\]\s/
+    var taskListRegex1 = /^\[ \]\s/;
+    var taskListRegex2 = /^\[x\]\s/;
     var _text = text.replace(taskListRegex1, '<input type="checkbox" disabled/> ')
                .replace(taskListRegex2, '<input type="checkbox" disabled checked/> ');
     var result = marked.Renderer.prototype.listitem(_text);
     if(_text !== text) {
       result = $(result).addClass('none-style')[0].outerHTML;
     }
-    return result
+    return result;
   }
   marked.setOptions({
     renderer: renderer,
