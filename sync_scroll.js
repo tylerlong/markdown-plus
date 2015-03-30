41ab352440342dd925c7ba0fa83997e2bf2fa1bd
Tyler Long <tyler4long@gmail.com>
2015-03-31 00:11:58 +0800

    Half finish sync_scroll

---
+++ sync_scroll.js
@@ -0,0 +1,66 @@
+// 思路： 找出所有的header line，计算出每一个的高度（从第一行开始算的高度）
+// 获取当前的scrollTop， 然后知道当前时在哪两个header之间，滚动的比例是多少
+// 右侧获取响应的header，滚动到响应的比例
+// editor.session.getRowLength(3) 获取指定行的行数。（考虑wrap因素）
+// editor.renderer.lineHeight 每一行所占高度。
+// editor.session.getScrollTop() 当前滚动到的位置。
+// editor.session.getScreenLength() 总的物理行数 考虑wrap因素
+
+
+//todo: 下划线创建的header没有考虑
+
+
+function get_editor_scroll() {
+  var lines = editor.getValue().split('\n');
+  var physicalLine = 0;
+  var headerLines = [];
+  for(var i = 0; i < lines.length; i++) {
+    if(/^#{1,4}\s+/.test(lines[i])) {
+      headerLines.push(physicalLine);
+    }
+    physicalLine += editor.session.getRowLength(i);
+  }
+
+  var currentLine = editor.session.getScrollTop() / editor.renderer.lineHeight;
+
+  var lastHeader = false;
+  var nextHeader = false;
+  for(var i = 0; i < headerLines.length; i++) {
+    if(headerLines[i] > currentLine) {
+      nextHeader = i;
+      break;
+    } else {
+      lastHeader = i;
+    }
+  }
+
+  var lastLine = 0;
+  var nextLine = physicalLine;
+  if(lastHeader !== false) {
+    lastLine = headerLines[lastHeader];
+  }
+  if(nextHeader !== false) {
+    nextLine = headerLines[nextHeader];
+  }
+
+  var percentage = (currentLine - lastLine) / (nextLine - lastLine);
+  console.log(percentage + ' between the ' + lastHeader + 'th and the ' + nextHeader + 'th headers');
+
+  var result = { lastHeader: lastHeader, nextHeader: nextHeader, percentage: percentage };
+  return result;
+}
+
+function set_preview_scroll(editor_scroll) {
+  console.log(editor_scroll);
+  var lastPosition = 0;
+  var nextPosition = $('.ui-layout-east article').outerHeight();
+  var headers = $('.ui-layout-east article').find('h1,h2,h3,h4');
+  if(editor_scroll.lastHeader !== false) {
+    lastPosition = headers[editor_scroll.lastHeader].position();
+  }
+  if(editor_scroll.nextHeader !== false) {
+    nextPosition = headers[editor_scroll.nextHeader].position();
+  }
+  console.log(lastPosition);
+  console.log(nextPosition);
+}
