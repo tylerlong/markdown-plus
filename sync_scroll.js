bf257be031013a44c78952f8dff4b5340c6f57ad
Tyler Long <tyler4long@gmail.com>
2015-03-31 18:12:44 +0800

    New sync_scroll methods

--- sync_scroll.js
+++ sync_scroll.js
@@ -7,60 +7,69 @@
 // editor.session.getScreenLength() 总的物理行数 考虑wrap因素
 
 
-//todo: 下划线创建的header没有考虑
-
-
 function get_editor_scroll() {
-  var lines = editor.getValue().split('\n');
-  var physicalLine = 0;
-  var headerLines = [];
-  for(var i = 0; i < lines.length; i++) {
-    if(/^#{1,6}\s+/.test(lines[i])) {
-      headerLines.push(physicalLine);
-    }
-    physicalLine += editor.session.getRowLength(i);
-  }
+  var headers = $('.ui-layout-east article').find('h1,h2,h3,h4,h5,h6').filter('[data-line]');
+  var lines = []; // 逻辑行
+  headers.each(function(){
+    lines.push($(this).data('line'));
+  });
+  console.log(lines);
 
-  var currentLine = editor.session.getScrollTop() / editor.renderer.lineHeight;
+  var pLines = []; // 物理行
+  var pLine = 0;
+  for(var i = 0; i < lines[lines.length - 1]; i++) {
+    if($.inArray(i + 1, lines) !== -1) {
+      pLines.push(pLine);
+    }
+    pLine += editor.session.getRowLength(i)
+  }
+  console.log(pLines);
+
+  var currentLine = editor.session.getScrollTop() / editor.renderer.lineHeight; // 物理行
+  console.log(currentLine);
 
   var lastHeader = false;
   var nextHeader = false;
-  for(var i = 0; i < headerLines.length; i++) {
-    if(headerLines[i] > currentLine) {
+  for(var i = 0; i < pLines.length; i++) {
+    if(pLines[i] < currentLine) {
+      lastHeader = i;
+    } else {
       nextHeader = i;
       break;
-    } else {
-      lastHeader = i;
     }
   }
-
   var lastLine = 0;
-  var nextLine = physicalLine;
+  var nextLine = editor.session.getScreenLength() - 1;
   if(lastHeader !== false) {
-    lastLine = headerLines[lastHeader];
+    lastLine = pLines[lastHeader];
   }
   if(nextHeader !== false) {
-    nextLine = headerLines[nextHeader];
+    nextLine = pLines[nextHeader];
+  }
+  console.log('lastLine: ' + lastLine);
+  console.log('currentLine: ' + currentLine);
+  console.log('nextLine: ' + nextLine);
+  var percentage = 0;
+  if(nextLine !== lastLine) { // 行首标题的情况下可能相等
+    percentage = (currentLine - lastLine) / (nextLine - lastLine);
   }
 
-  var percentage = (currentLine - lastLine) / (nextLine - lastLine);
-  console.log(percentage + ' between the ' + lastHeader + 'th and the ' + nextHeader + 'th headers');
-
-  var result = { lastHeader: lastHeader, nextHeader: nextHeader, percentage: percentage };
+  var result = { lastHeader: lines[lastHeader], nextHeader: lines[nextHeader], percentage: percentage };
   return result;
 }
 
 function set_preview_scroll(editor_scroll) {
+  console.log(editor_scroll);
   var lastPosition = 0;
   var nextPosition = $('.ui-layout-east article').outerHeight() - $('.ui-layout-east').height();
-  var headers = $('.ui-layout-east article').find('h1,h2,h3,h4,h5,h6');
-  if(editor_scroll.lastHeader !== false) {
-    lastPosition = headers.eq(editor_scroll.lastHeader).get(0).offsetTop;
+  if(editor_scroll.lastHeader !== undefined) {
+    lastPosition = $('.ui-layout-east article').find('h1,h2,h3,h4,h5,h6').filter('[data-line="' + editor_scroll.lastHeader + '"]').get(0).offsetTop;
   }
-  if(editor_scroll.nextHeader !== false) {
-    nextPosition = headers.eq(editor_scroll.nextHeader).get(0).offsetTop;
+  if(editor_scroll.nextHeader !== undefined) {
+    nextPosition = $('.ui-layout-east article').find('h1,h2,h3,h4,h5,h6').filter('[data-line="' + editor_scroll.nextHeader + '"]').get(0).offsetTop;
   }
+  console.log(lastPosition);
+  console.log(nextPosition);
   scrollPosition = lastPosition + (nextPosition - lastPosition) * editor_scroll.percentage;
-  console.log(scrollPosition);
-  $('.ui-layout-east').animate({scrollTop: scrollPosition}, 256);
+  $('.ui-layout-east').animate({scrollTop: scrollPosition}, 128);
 }
