fd656be7537f8480828a4f6aa4c0f687dca4c47b
Tyler Long <tyler4long@gmail.com>
2016-02-13 21:37:26 +0800

    Fix anchor link scroll issue

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(function() {
   $.get('sample.md', function(data) { // load sample text
     editor.session.setValue(data, -1);
+    setTimeout(function() {
+      window.addEventListener("hashchange", function() {
+        $('.ui-layout-east').scrollTop($('.ui-layout-east').scrollTop() - 6);
+      }); // a little gap to top
+      if(window.location.hash.length > 0) {
+        $('.ui-layout-east').scrollTop($(window.location.hash).offset().top - 30); // scroll to hash element
+      }
+    }, 1000);
   });
 });
