4098f390abe88a393723ffe929b2aaa1bf38312e
Tyler Liu <tyler4long@gmail.com>
2024-10-06 17:42:07 -0700

    Upgrade webpack to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,65 @@
-import path from 'path'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const rules = [
   {
     test: /\.css$/,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: 'css-loader'
-    })
+    use: [MiniCssExtractPlugin.loader, "css-loader"],
   },
   {
     test: /\.js$/,
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
         presets: [
           [
-            'env',
+            "env",
             {
-              'targets': {
-                'browsers': ['last 2 versions']
-              }
-            }
-          ]
+              targets: {
+                browsers: ["last 2 versions"],
+              },
+            },
+          ],
         ],
         plugins: [
-          'transform-remove-strict-mode' // in order to make mermaid work
-        ]
-      }
-    }
-  }
-]
+          "transform-remove-strict-mode", // in order to make mermaid work
+        ],
+      },
+    },
+  },
+];
 
 const config = {
-  target: 'web',
+  mode: "development",
+  target: "web",
   entry: {
-    'index': './src/index.js'
+    index: "./src/index.js",
   },
-  externals: 'fs', // in order to make mermaid work
+  externals: "fs", // in order to make mermaid work
   output: {
-    path: path.join(__dirname, './docs/'),
-    filename: '[name].bundle.js'
+    path: path.join(__dirname, "./docs/"),
+    filename: "[name].bundle.js",
   },
   module: { rules },
   plugins: [
-    new ExtractTextPlugin('[name].bundle.css')
-  ]
-}
+    new MiniCssExtractPlugin({ filename: "[name].bundle.css" }),
+    new HtmlWebpackPlugin({
+      template: "./docs/index.html",
+      inject: "head",
+      scriptLoading: "blocking",
+    }),
+  ],
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "docs"), // Directory to serve static files from
+    },
+  },
+};
 
-export default [config]
+export default [config];
