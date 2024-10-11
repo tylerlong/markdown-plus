9bd5865286a4123079e8b88e3371b52c518e51a5
Tyler Liu <tyler4long@gmail.com>
2024-10-10 17:38:15 -0700

    Minor change

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,13 @@ root.render(
       token: {
         colorPrimary: '#00b96b',
       },
+      components: {
+        Splitter: {
+          splitBarSize: 6,
+          resizeSpinnerSize: 64,
+          splitTriggerSize: 12,
+        },
+      },
     }}
   >
     <App store={store} />
